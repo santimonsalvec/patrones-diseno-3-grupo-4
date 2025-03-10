732071737c48e0f6469013160d7339319e645809
Ricardo Andres Delgado Betancur <ricardoandres67@hotmail.com>
2025-03-10 17:12:33 -0500

    Proyecto terminado

---
+++ Clonar_PersonajesV1/Clonar_PersonajesV1/Heroes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clonar_PersonajesV1
+{
+	internal class Heroes : ICloneable
+	{
+		public string Nombre { get; set; }
+		public string ClaseHeroe { get; set; }
+		public List<string> Habilidades { get; set; }
+		public int Experiencia { get; set; }
+		public int Nivel { get; set; }
+		public int PuntosHabilidad { get; set; }
+
+		
+		public Heroes(string nombre, string claseHeroe, List<string> habilidades, int experiencia = 0, int nivel = 1, int puntosHabilidad = 0)
+		{
+			Nombre = nombre;
+			ClaseHeroe = claseHeroe;
+			Habilidades = new List<string>(habilidades); 
+			Experiencia = experiencia;
+			Nivel= nivel;
+			PuntosHabilidad = puntosHabilidad;
+		}
+
+		// Método para clonar el héroe
+		public object Clone()
+		{
+			// Realiza una clonación profunda
+			return new Heroes(Nombre, ClaseHeroe, new List<string>(Habilidades), Experiencia, Nivel, PuntosHabilidad);
+		}
+
+		public void Personalizar(string nuevoNombre, string nuevaApariencia)
+		{
+			Nombre = nuevoNombre;
+			Console.WriteLine($"Hereo {Nombre}  Nueva Apariencia o Aspecto: {nuevaApariencia}");
+		}
+
+		// Método para mostrar detalles del héroe
+		public override string ToString()
+		{
+			return $"{Nombre} ({ClaseHeroe}) - Nivel: {Nivel}, Experiencia: {Experiencia}, PuntosHabilidad: {PuntosHabilidad}, Habilidades: {string.Join(", ", Habilidades)}";
+		}
+	}
+
+}
+
---
+++ Clonar_PersonajesV1/Clonar_PersonajesV1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clonar_PersonajesV1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var personaje = new Heroes("Hercules", "Gladiador", new List<string> { "Fuerza Ilimitada", "Luchador Agil" }, experiencia: 100, nivel: 5, puntosHabilidad: 10);
+
+			var ClonarPersonaje = (Heroes)personaje.Clone();
+
+			ClonarPersonaje.Personalizar("Optimus", "Transformer");
+
+			Console.WriteLine("Hereo Original:");
+			Console.WriteLine(personaje);
+
+			Console.WriteLine("\nHeroe Clonado:");
+			Console.WriteLine(ClonarPersonaje);
+
+			ClonarPersonaje.Habilidades.Add("Dispara cohetes");
+
+			Console.WriteLine("\nNuevas Habilidades despues del clonaje:");
+			Console.WriteLine("Heroe Original:");
+			Console.WriteLine(personaje);
+
+			Console.WriteLine("\nHeroe Clonado:");
+			Console.WriteLine(ClonarPersonaje);
+		}
+	}
+}
+	
